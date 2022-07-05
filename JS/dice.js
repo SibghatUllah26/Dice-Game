03638d10b070ca83c5ae3c958afc990f0ed93b4d
SibghatUllah <95136091+SibghatUllah26@users.noreply.github.com>
2022-07-05 12:40:02 +0500

    update

--- JS/dice.js
+++ JS/dice.js
@@ -1,3 +1,7 @@
+var i=0;
+var j=0;
+var k=0;
+var m=0;
 document.querySelector(".btn").addEventListener("click", function(){
   var audio = new Audio("images/audio.mp3");
   audio.play();
@@ -9,8 +13,18 @@ var randomImage2 = "images/dice"+randomNumber2+".png";
 var image2 = document.querySelector(".img2").setAttribute("src",randomImage2);
 if (randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML  = " 🚩 Player 1 Wins!";
+  var player1 = ++j;
+  document.querySelectorAll("h2")[0].innerHTML = "Player 1 Won = " + player1;
 }else if (randomNumber1 < randomNumber2){
   document.querySelector("h1").innerHTML  = "  Player 2 Wins! 🚩";
+  var player2 = ++k;
+  document.querySelectorAll("h2")[3].innerHTML = "Player 2 Won = " + player2;
 }else if (randomNumber1=== randomNumber2){
   document.querySelector("h1").innerHTML  = "It's a draw 🎌 ";
-}});
+  var draw = ++m;
+  document.querySelectorAll("h2")[2].innerHTML = "Draw = " + draw;
+
+}
+var count = ++i;
+document.querySelectorAll("h2")[1].innerHTML = "Total Dice Roll = " + count;
+});
